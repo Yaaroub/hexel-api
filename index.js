b54303f687db4329b1a1ab0217744b864cbb7a60
Yaaroub Al Haj Dawoud <ydawoud@MB-492351.local>
2025-05-03 17:59:35 +0200

    srfe

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ app.post('/api/contact', async (req, res) => {
   // Mail Transporter konfigurieren
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST || 'smtp.ionos.de',
-    port: parseInt(process.env.EMAIL_PORT || '587'),
-    secure: false,
+    port: parseInt(process.env.EMAIL_PORT || '465'),
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
